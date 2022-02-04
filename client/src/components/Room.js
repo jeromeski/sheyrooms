bc71a7f09031db3afd85b6f11d410c1e779bb3e0
jeromeski <jeromegee@gmail.com>
2022-02-05 06:20:24 +0800

    Room content and styling

---
+++ client/src/components/Room.js
@@ -0,0 +1,24 @@
+import { Button } from "react-bootstrap";
+
+export default function Room(props) {
+	const { name, phonenumber, description, maxcount, rentperday, type, imageurls } = props;
+	return (
+		<div className="row bg-light mb-5">
+			<div className="col-md-4">
+				<img src={imageurls[0]} alt={name} className="small-img" />
+			</div>
+			<div className="col-md-8 p-2">
+				<h1>{name}</h1>
+				<p>
+					Number of Guests: <b>{maxcount}</b>
+				</p>
+				<p>Contact Number:{phonenumber}</p>
+				<p>{type}</p>
+				<div style={{ float: "right" }}>
+					<button className="btn btn-dark">Book Now</button>
+					<button className="btn btn-dark">View Details</button>
+				</div>
+			</div>
+		</div>
+	);
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import Room from '../components/Room';
 import { useAxios } from "../hooks/useAxios";
 
 const url = process.env.REACT_APP_BACKEND_URL;
 
 function Home() {
-	const [rooms, setRooms] = useState([]);
 	const { data, isLoading, error } = useAxios(`${url}/rooms`);
+	const [show, setShow] = useState(false);
+	const [rooms, setRooms] = useState([]);
 
-	console.log(error);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	useEffect(() => {
 		let current = true;
@@ -17,13 +21,63 @@ function Home() {
 		return () => (current = false);
 	}, [data]);
 
-	return isLoading ? (
-		<h1>Loading...</h1>
-	) : (
-		<div>
-			{data && rooms.map((room) => <pre key={room._id}>{JSON.stringify(room, null, 2)}</pre>)}
+	return (
+		<div className="container">
+			<div className="row justify-content-center mt-5">
+				{isLoading ? (
+					<h1>Loading...</h1>
+				) : error ? (
+					<h1>Error Fetching Rooms...</h1>
+				) : (
+					<>
+						{rooms.map((room) => (
+							<div key={room._id} className="col-md-9">
+								<Room {...room} />
+							</div>
+						))}
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
 
 export default Home;
+
+/*
+	<Button variant="primary" onClick={handleShow}>
+						Launch demo modal
+					</Button>
+
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>Modal heading</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+						<Modal.Footer>
+							<Button variant="secondary" onClick={handleClose}>
+								Close
+							</Button>
+							<Button variant="primary" onClick={handleClose}>
+								Save Changes
+							</Button>
+						</Modal.Footer>
+					</Modal>	<Button variant="primary" onClick={handleShow}>
+						Launch demo modal
+					</Button>
+
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>Modal heading</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+						<Modal.Footer>
+							<Button variant="secondary" onClick={handleClose}>
+								Close
+							</Button>
+							<Button variant="primary" onClick={handleClose}>
+								Save Changes
+							</Button>
+						</Modal.Footer>
+					</Modal>
+*/ 
